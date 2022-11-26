0f23b1b274d3b6f71447142f0dd143c45bc45f4e
Mateusz <balcerzakmateusz2@gmail.com>
2022-11-26 13:31:17 +0100

    Createnext gamesite

--- nextjsshop/pages/index.tsx
+++ nextjsshop/pages/index.tsx
@@ -4,10 +4,22 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <div className={styles.container}>
       <Head>
         <title>Nextshop</title>
         <meta name="description" content="Nextjsshop" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={styles.headercontainer}>
+        <img className={styles.img} src="" alt="" />
+        <h1 className={styles.shopname}>Shopname</h1>
+        <div className={styles.basket}></div>
+      </header>
+      <section>
+      </section>
+      <footer>
+
+      </footer>
+      </div>
   )
 }
